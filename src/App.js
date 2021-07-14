0ec3de03dac4a6a3c715944d5fb85ef789c3a06c
Steve Munoz <stevomunoz@yahoo.com>
2021-07-13 20:00:56 -0500

    create react project router setup

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react"
+import {Switch, Route} from "react-router-dom"
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import TrailResults from "./components/TrailResults"
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+
+      <Switch>
+
+        <Route exact path ="/">
+          <Home/>
+        </Route>
+
+        <Route path ="/trailresults">
+          <TrailResults/>
+        </Route>
+
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrailResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TrailResults(){
+    return(
+        <div>
+            Results Page
+        </div>
+
+    )
+}
+
+export default TrailResults
