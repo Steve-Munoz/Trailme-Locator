744e916176216a0168681f5828ea47df565861a8
Steve Munoz <stevomunoz@yahoo.com>
2021-07-14 10:38:04 -0500

    create header

--- src/components/TrailResults.js
+++ src/components/TrailResults.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function TrailResults(){
     return(
         <div>
             Results Page
+            <button><Link to ="/trailresults"><i class="ri-search-line"></i></Link></button>
         </div>
 
     )
